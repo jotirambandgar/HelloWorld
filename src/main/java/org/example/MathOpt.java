3cce72989bed1d1392233f46718a5a507ae71d6d
jotirambandgar <bandgar09@gmail.com>
2023-01-19 23:45:58 +0530

    Added new changes

---
+++ src/main/java/org/example/MathOpt.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class MathOpt {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("a + b = " + (a + b) );
+    }
+}
